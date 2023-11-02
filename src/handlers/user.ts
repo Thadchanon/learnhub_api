a1a2e10d59adbc210c3f60747855ad3dcbbe3b09
Thadchanon <thadchanonjirarat@gmail.com>
2023-11-02 11:07:34 +0700

    add feat/user-registration

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,3 +1,65 @@
+import { RequestHandler } from "express";
 import { IUserHandler } from ".";
+import { ICreateUserDto, IUserDto } from "../dto/user";
+import { IErrorDto } from "../dto/error";
+import { IUserRepository, UserCreationError } from "../repositories";
+import { hashPassword } from "../utils/bcrypt";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-export default class UserHandler implements IUserHandler {}
+export default class UserHandler implements IUserHandler {
+  private repo: IUserRepository;
+  constructor(repo: IUserRepository) {
+    this.repo = repo;
+  }
+  public registration: RequestHandler<
+    {},
+    IUserDto | IErrorDto,
+    ICreateUserDto
+  > = async (req, res) => {
+    /*    const name = req.body.name;
+    const username = req.body.username */
+    const { name, username, password: plainPassword } = req.body;
+
+    if (typeof name !== "string" || name.length === 0)
+      return res.status(400).json({ message: "name is invalid" });
+    if (typeof username !== "string" || username.length === 0)
+      return res.status(400).json({ message: "username is invalid" });
+    if (typeof plainPassword !== "string" || plainPassword.length < 5)
+      return res.status(400).json({ message: "password is invalid" });
+
+    try {
+      const {
+        id: registeredId,
+        name: registeredName,
+        registeredAt,
+        username: registeredUsername,
+      } = await this.repo.create({
+        name,
+        username,
+        password: hashPassword(plainPassword),
+      });
+
+      return res
+        .status(201)
+        .json({
+          id: registeredId,
+          name: registeredName,
+          registeredAt: `${registeredAt}`,
+          username: registeredUsername,
+        })
+        .end();
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      ) {
+        return res.status(500).json({
+          message: `name is invalid`,
+        });
+      }
+      return res.status(500).json({
+        message: `Internal Server Error`,
+      });
+    }
+  };
+}
